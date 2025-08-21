0a4dd39b2e2260ee4f8f5780c6053b3f5b2e6505
dlwjddus1112 <dlwjddus1112@naver.com>
2025-08-21 17:18:13 +0900

    [Init] DockerFile 추가

---
+++ src/main/java/com/example/FC_BACKEND/FcBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.FC_BACKEND;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FC_BACKEND/FcBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FC_BACKEND;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
