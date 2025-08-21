04cb4471ec0809a3fc24aceb8a0707d7caae3ac2
dlwjddus1112 <dlwjddus1112@naver.com>
2025-08-21 20:50:12 +0900

    [Feat] 공통 응답 형식 설정

--- src/main/java/com/example/FC_BACKEND/global/config/WebConfig.java
+++ src/main/java/com/example/FC_BACKEND/global/config/WebConfig.java
@@ -2,11 +2,9 @@ package com.example.FC_BACKEND.global.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
