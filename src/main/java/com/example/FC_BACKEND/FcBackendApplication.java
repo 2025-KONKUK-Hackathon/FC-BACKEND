60ee156403971cc69865020af66f019c297aaea1
dlwjddus1112 <dlwjddus1112@naver.com>
2025-08-22 11:28:57 +0900

    [Feat] 유저 도메인 구현

--- src/main/java/com/example/FC_BACKEND/FcBackendApplication.java
+++ src/main/java/com/example/FC_BACKEND/FcBackendApplication.java
@@ -2,7 +2,9 @@ package com.example.FC_BACKEND;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class FcBackendApplication {
 
---
+++ src/main/java/com/example/FC_BACKEND/domain/user/entity/User.java
@@ -0,0 +1,42 @@
+package com.example.FC_BACKEND.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    private String studentNumber;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
