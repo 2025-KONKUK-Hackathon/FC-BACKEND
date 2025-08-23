3e0cf1b79e3efed7cfb78ea97526e2721d091388
dlwjddus1112 <dlwjddus1112@naver.com>
2025-08-23 20:42:32 +0900

    [Feat] SecurityConfig 구현

---
+++ src/main/java/com/example/FC_BACKEND/global/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.example.FC_BACKEND.global.config;
+
+import com.example.FC_BACKEND.global.auth.jwt.CustomAccessDeniedHandler;
+import com.example.FC_BACKEND.global.auth.jwt.CustomAuthenticationEntryPoint;
+import com.example.FC_BACKEND.global.auth.jwt.JwtAuthenticationFilter;
+import com.example.FC_BACKEND.global.auth.jwt.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtUtil jwtUtil;
+
+    public static final String[] ALLOWED_PATHS = {
+            "/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/swagger-config",
+            "/users/signup", "/users/email/verification-requests", "/users/email/verifications"
+    };
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers(ALLOWED_PATHS).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(new JwtAuthenticationFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(auth -> auth
+                        .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+                        .accessDeniedHandler(new CustomAccessDeniedHandler())
+                );
+
+        return http.build();
+    }
+}
