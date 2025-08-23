8470db3b5249aeb3c72b33297288f8f4032218f8
dlwjddus1112 <dlwjddus1112@naver.com>
2025-08-23 13:53:18 +0900

    [Feat] 모임 컨트롤러, 서비스, 리포지토리 구현

---
+++ src/main/java/com/example/FC_BACKEND/domain/meeting/controller/MeetingController.java
@@ -0,0 +1,11 @@
+package com.example.FC_BACKEND.domain.meeting.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("meetings")
+public class MeetingController {
+}
