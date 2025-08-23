ade95d2af89d78863c36e022dbc0190efe2fd7e2
dlwjddus1112 <dlwjddus1112@naver.com>
2025-08-23 13:03:04 +0900

    [Feat] Post 도메인 작성

---
+++ src/main/java/com/example/FC_BACKEND/domain/post/entity/Post.java
@@ -0,0 +1,53 @@
+package com.example.FC_BACKEND.domain.post.entity;
+
+import com.example.FC_BACKEND.domain.post.constant.Grade;
+import com.example.FC_BACKEND.domain.post.constant.Part;
+import com.example.FC_BACKEND.domain.post.constant.Topic;
+import com.example.FC_BACKEND.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Part part;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Grade grade;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Topic topic;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+}
