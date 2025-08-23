4d468d7fbc6edec28eb92191109a66cdb15519dd
dlwjddus1112 <dlwjddus1112@naver.com>
2025-08-23 13:09:28 +0900

    [Feat] Comment 도메인 구현

---
+++ src/main/java/com/example/FC_BACKEND/domain/comment/entity/Comment.java
@@ -0,0 +1,35 @@
+package com.example.FC_BACKEND.domain.comment.entity;
+
+import com.example.FC_BACKEND.domain.post.entity.Post;
+import com.example.FC_BACKEND.domain.user.entity.User;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(nullable = false)
+    private String content;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+}
