6ebfa467212a9eaf517ea50bb4c46c9b900c7378
dlwjddus1112 <dlwjddus1112@naver.com>
2025-08-23 13:09:46 +0900

    [Feat] CommentRepository, CommentService 구현

---
+++ src/main/java/com/example/FC_BACKEND/domain/comment/service/CommentService.java
@@ -0,0 +1,9 @@
+package com.example.FC_BACKEND.domain.comment.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+}
