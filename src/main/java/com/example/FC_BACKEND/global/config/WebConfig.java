7fdb96817aaa2d0e4d81da14899aacd83e68812c
dlwjddus1112 <dlwjddus1112@naver.com>
2025-08-23 20:43:00 +0900

    [Feat] WebConfig에 ArgumentResolver 설정 추가

--- src/main/java/com/example/FC_BACKEND/global/config/WebConfig.java
+++ src/main/java/com/example/FC_BACKEND/global/config/WebConfig.java
@@ -1,15 +1,20 @@
 package com.example.FC_BACKEND.global.config;
 
+import com.example.FC_BACKEND.global.auth.jwt.LoginArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
+    private final LoginArgumentResolver loginArgumentResolver;
 
     private static final String frondEndOrigin = "https://fc-frontend-zeta.vercel.app";
 
@@ -21,6 +26,10 @@ public class WebConfig implements WebMvcConfigurer {
 
     private static final String httpPrefix = "http://";
 
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginArgumentResolver);
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
