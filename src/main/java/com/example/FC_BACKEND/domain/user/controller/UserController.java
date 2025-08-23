f6d4120fd7eef5ea6e193e369d4afbcc6800bbfc
dlwjddus1112 <dlwjddus1112@naver.com>
2025-08-23 17:09:12 +0900

    [Feat] 이메일 인증 컨트롤러 로직 구현

--- src/main/java/com/example/FC_BACKEND/domain/user/controller/UserController.java
+++ src/main/java/com/example/FC_BACKEND/domain/user/controller/UserController.java
@@ -1,11 +1,41 @@
 package com.example.FC_BACKEND.domain.user.controller;
 
+import com.example.FC_BACKEND.domain.user.dto.request.EmailRequest;
+import com.example.FC_BACKEND.domain.user.service.UserService;
+import com.example.FC_BACKEND.global.annotation.CustomExceptionDescription;
+import com.example.FC_BACKEND.global.config.swagger.SwaggerResponseDescription;
+import com.example.FC_BACKEND.global.response.BaseResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import static com.example.FC_BACKEND.global.config.swagger.SwaggerResponseDescription.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("users")
 public class UserController {
+
+    private final UserService userService;
+
+    @Tag(name = "회원가입 관련 API")
+    @Operation(summary = "이메일 인증번호 전송")
+    @CustomExceptionDescription(SEND_EMAIL_CODE)
+    @PostMapping("emails/verification-requests")
+    public BaseResponse<Void> sendEmail(@RequestBody EmailRequest req){
+        userService.sendCodeToEmail(req.email());
+        return BaseResponse.ok(null, "인증번호가 전송되었습니다.");
+    }
+
+    @Tag(name = "회원가입 관련 API")
+    @Operation(summary = "인증번호 확인")
+    @CustomExceptionDescription(VERIFY_CODE)
+    @GetMapping("emails/verifications")
+    public BaseResponse<Void> verify(@RequestParam String email, @RequestParam String code){
+        userService.verifyCode(email,code);
+        return BaseResponse.ok(null,"인증에 성공하였습니다.");
+    }
+
+
 }
