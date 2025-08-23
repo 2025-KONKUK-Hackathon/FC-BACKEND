c5cd45bcfd80265c29985b0858d83b53669c2c8d
dlwjddus1112 <dlwjddus1112@naver.com>
2025-08-23 17:08:36 +0900

    [Feat] 이메일 인증번호 전송 및 인증번호 검사 비즈니스 로직 구현

--- src/main/java/com/example/FC_BACKEND/domain/user/repository/UserRespository.java
+++ src/main/java/com/example/FC_BACKEND/domain/user/repository/UserRespository.java
@@ -3,5 +3,9 @@ package com.example.FC_BACKEND.domain.user.repository;
 import com.example.FC_BACKEND.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRespository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
--- src/main/java/com/example/FC_BACKEND/domain/user/service/UserService.java
+++ src/main/java/com/example/FC_BACKEND/domain/user/service/UserService.java
@@ -1,9 +1,82 @@
 package com.example.FC_BACKEND.domain.user.service;
 
+import com.example.FC_BACKEND.domain.user.repository.UserRespository;
+import com.example.FC_BACKEND.global.email.entity.EmailVerifyCode;
+import com.example.FC_BACKEND.global.email.repository.EmailVerifyCodeRepository;
+import com.example.FC_BACKEND.global.exception.customexception.CustomException;
+import com.example.FC_BACKEND.global.email.service.EmailService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Random;
+
+import static com.example.FC_BACKEND.global.exception.constant.GlobalErrorCode.*;
+import static com.example.FC_BACKEND.global.exception.constant.UserErrorCode.*;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class UserService {
+
+    private final UserRespository userRespository;
+
+    private final EmailVerifyCodeRepository emailVerifyCodeRepository;
+
+    private static final String VALID_EMAIL_SUFFIX = "konkuk.ac.kr";
+
+    private final EmailService emailService;
+
+    @Transactional
+    public void sendCodeToEmail(String toEmail) {
+        validateEmail(toEmail);
+        String title = "[Feat/Connect] 이메일 인증 번호";
+        String authCode = this.createCode();
+
+        emailService.sendEmail(toEmail, title, authCode);
+        Optional<EmailVerifyCode> verifyCode = emailVerifyCodeRepository.findByEmail(toEmail);
+        if(verifyCode.isPresent()){
+            EmailVerifyCode code = verifyCode.get();
+            code.setVerifyCode(authCode);
+            return;
+        }
+        emailVerifyCodeRepository.save(EmailVerifyCode.create(toEmail, authCode));
+    }
+
+    public void verifyCode(String email, String code){
+        EmailVerifyCode verifyCode = emailVerifyCodeRepository.findByEmail(email).orElseThrow(() -> new CustomException(VERIFY_CODE_NOT_FOUND));
+        if(!Objects.equals(verifyCode.getVerifyCode(), code)) throw new CustomException(VERIFY_CODE_MISMATCH);
+
+    }
+
+    private String createCode() {
+        int lenth = 6;
+        try {
+            Random random = SecureRandom.getInstanceStrong();
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < lenth; i++) {
+                builder.append(random.nextInt(10));
+            }
+            return builder.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new CustomException(INTERNAL_SERVER_ERROR, e.getMessage());
+        }
+    }
+
+    private void validateEmail(String email) {
+        String emailRegex = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$";
+        if(!email.matches(emailRegex)) throw new CustomException(INVALID_EMAIL_TYPE);
+
+        if(!email.endsWith(VALID_EMAIL_SUFFIX)) throw new CustomException(INVALID_EMAIL);
+
+        if(userRespository.existsByEmail(email)) throw new CustomException(EMAIL_DUPLICATE);
+
+    }
+
 }
