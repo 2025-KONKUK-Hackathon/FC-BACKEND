0b2341eba52f7ab70bb60d831267ff96db5e36a0
dlwjddus1112 <dlwjddus1112@naver.com>
2025-08-23 13:53:04 +0900

    [Feat] 모임 카테고리 ENUM 추가

---
+++ src/main/java/com/example/FC_BACKEND/domain/meeting/constant/Category.java
@@ -0,0 +1,12 @@
+package com.example.FC_BACKEND.domain.meeting.constant;
+
+import lombok.Getter;
+
+@Getter
+public enum Category {
+    STUDY,
+    PROJECT,
+    EVENT,
+    FRIENDSHIP,
+    ETC
+}
