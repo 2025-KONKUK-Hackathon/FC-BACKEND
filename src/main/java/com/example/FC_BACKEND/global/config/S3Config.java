848618238d1cb1ad1161b7b83ae852d3c693b005
dlwjddus1112 <dlwjddus1112@naver.com>
2025-08-24 17:44:46 +0900

    [Feat] Presigner 설정 추가

--- src/main/java/com/example/FC_BACKEND/global/config/S3Config.java
+++ src/main/java/com/example/FC_BACKEND/global/config/S3Config.java
@@ -4,10 +4,15 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Slf4j
 @Configuration
@@ -21,12 +26,17 @@ public class S3Config {
     @Value("${cloud.aws.region.static}")
     private String region;
 
+    @Getter
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
     @Bean
-    public AmazonS3Client amazonS3Client() {
-        BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
-        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
-                .withRegion(region)
-                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+    public S3Presigner s3Presigner() {
+        return S3Presigner.builder()
+                .region(Region.of(region))
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(
+                                AwsBasicCredentials.create(accessKey, secretKey)))
                 .build();
     }
 }
