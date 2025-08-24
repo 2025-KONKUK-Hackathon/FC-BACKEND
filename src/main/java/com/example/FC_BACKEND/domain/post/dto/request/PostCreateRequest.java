94d15dbff9dece024a515b32b3e40ee41d755afc
dlwjddus1112 <dlwjddus1112@naver.com>
2025-08-24 18:46:20 +0900

    [Feat] 게시글 생성 요청 DTO 구현

---
+++ src/main/java/com/example/FC_BACKEND/domain/post/dto/request/PostCreateRequest.java
@@ -0,0 +1,30 @@
+package com.example.FC_BACKEND.domain.post.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.util.List;
+
+public record PostCreateRequest(
+
+        @Schema(description = "제목", example = "아니")
+        String title,
+
+        @Schema(description = "내용", example = "서버 한명 더 구할걸 그냥 아")
+        String content,
+
+        @Schema(description = "이미지")
+        List<String> imageUrls,
+
+        @Schema(description = "파트", example = "SERVER")
+        String part,
+
+        @Schema(description = "학년", example = "FOURTH")
+        String grade,
+
+        @Schema(description = "주제", example = "CLASS")
+        String Topic,
+
+        @Schema(description = "소속", example = "COMPUTER")
+        String affiliation
+) {
+}
