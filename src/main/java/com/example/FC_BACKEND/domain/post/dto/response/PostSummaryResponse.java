b3495860520682371fb56fdb161496e3908a62db
dlwjddus1112 <dlwjddus1112@naver.com>
2025-08-24 22:02:01 +0900

    [Feat] 게시물 목록 조회 DTO 구현

---
+++ src/main/java/com/example/FC_BACKEND/domain/post/dto/response/PostSummaryResponse.java
@@ -0,0 +1,41 @@
+package com.example.FC_BACKEND.domain.post.dto.response;
+
+import com.example.FC_BACKEND.global.dto.CursorProvider;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.time.LocalDateTime;
+
+public record PostSummaryResponse(
+        @Schema(description = "게시글 ID")
+        Long postId,
+
+        @Schema(description = "작성자 ID")
+        Long writerId,
+
+        @Schema(description = "제목", example = "아니")
+        String title,
+
+        @Schema(description = "내용", example = "진짜 서버 한 명만 더..")
+        String content,
+
+        @Schema(description = "댓글 수")
+        int commentCount,
+
+        @Schema(description = "썸네일 이미지 url")
+        String imageUrl,
+
+        @Schema(description = "작성 시간")
+        LocalDateTime createdAt
+) implements CursorProvider<Long>
+{
+
+
+    public static PostSummaryResponse of(Long postId, Long writerId, String title, String content, int commentCount, String imageUrl, LocalDateTime createdAt) {
+        return new PostSummaryResponse(postId, writerId, title, content, commentCount, imageUrl, createdAt);
+    }
+
+    @Override
+    public Long getCursor() {
+        return postId;
+    }
+}
