fab7933c39e737a171916a0a885087e105d65e9f
dlwjddus1112 <dlwjddus1112@naver.com>
2025-08-26 23:31:53 +0900

    [Feat] 모임 참여자 조회 DTO 구현

---
+++ src/main/java/com/example/FC_BACKEND/domain/meeting/dto/response/MeetingMemberResponse.java
@@ -0,0 +1,18 @@
+package com.example.FC_BACKEND.domain.meeting.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record MeetingMemberResponse(
+        @Schema(description = "모임 멤버 ID")
+        Long meetingMemberId,
+
+        @Schema(description = "모임 멤버 이름")
+        String name,
+
+        @Schema(description = "모임 멤버 전화번호")
+        String phone,
+
+        @Schema(description = "모임 멤버 학번")
+        String studentNunmber
+) {
+}
