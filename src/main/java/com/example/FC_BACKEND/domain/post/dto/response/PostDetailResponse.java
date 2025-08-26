a1b3c087eefde4c2ce1a165007f974be6fd62d93
dlwjddus1112 <dlwjddus1112@naver.com>
2025-08-26 20:24:07 +0900

    [Feat] 게시글 상세 조회 응답 DTO 구현

---
+++ src/main/java/com/example/FC_BACKEND/domain/post/dto/response/PostDetailResponse.java
@@ -0,0 +1,30 @@
+package com.example.FC_BACKEND.domain.post.dto.response;
+
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
+@Builder
+public record PostDetailResponse(
+        @Schema(description = "작성자 이름", example = "이정연")
+        String writerName,
+
+        @Schema(description = "작성자 ID")
+        Long writerId,
+
+        @Schema(description = "제목", example = "ㅇㅇ")
+        String title,
+
+        @Schema(description = "내용", example = "ㅇㅇㅇ")
+        String content,
+
+        @Schema(description = "작성 시간")
+        LocalDateTime createdAt,
+
+        @Schema(description = "댓글 수", example = "1")
+        int commentCount
+
+) {
+}
